4f3158fe10661909e912fe3d66463b13e67dd60a
J2PIn <64399278+J2PIn@users.noreply.github.com>
2026-02-10 19:38:18 +0200

    Add HeartbeatDO class and fetch method

--- worker.js
+++ worker.js
@@ -1,1 +1,16 @@
+export default {
+  async fetch(request, env) {
+    const id = env.HEARTBEAT.idFromName("tenant:public");
+    return env.HEARTBEAT.get(id).fetch(request);
+  },
+};
 
+export class HeartbeatDO {
+  constructor(state, env) {
+    this.state = state;
+    this.env = env;
+  }
+  async fetch() {
+    return new Response("ok");
+  }
+}
